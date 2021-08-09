0d7edc1fcaf84995141ae253f11b1a27e843e131
sangjun <sgbo5003@naver.com>
2021-08-09 17:27:39 +0900

    코인 버튼 컴포넌트 생성

---
+++ src/components/CoinButton.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import coinMenu from "../images/Context_menu_icon1.png";
+import CoinAttdModal from "./CoinAttdModal";
+
+const CoinButton = () => {
+  const [coinModalOn, setCoinModalOn] = useState(false);
+
+  const [attdCheckedItems, setAttdCheckedItems] = useState(new Set());
+
+  const coinModalHandler = () => {
+    setCoinModalOn(!coinModalOn);
+  };
+  return (
+    <>
+      <div class="coin_pre_btn_cove" onClick={coinModalHandler}>
+        <span class="coin_pre_btn">
+          <img
+            class="coin_pre_btn_img"
+            src={coinMenu}
+            alt="Context_menu_icon1.png"
+          />
+        </span>
+        <span class="coin_pre_text">출석 코인받기</span>
+      </div>
+      {coinModalOn ? (
+        <CoinAttdModal
+          coinModalHandler={coinModalHandler}
+          attdCheckedItems={attdCheckedItems}
+          setAttdCheckedItems={setAttdCheckedItems}
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default CoinButton;
