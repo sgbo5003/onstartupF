96c280a2ecf39127a0f4543c4ba411aca07dd191
sangjun <sgbo5003@naver.com>
2021-08-09 17:27:23 +0900

    문의하기 선택창 모 화면 생성

---
+++ src/components/InquirySelectModal.js
@@ -0,0 +1,45 @@
+import React from "react";
+import BackImg from "../images/back.png";
+
+const InquirySelectModal = (props) => {
+  const arr = [1, 2, 3, 4, 5];
+  const { selectModalHandler, setCategory } = props;
+  const selectList = arr.map((data) => {
+    return (
+      <li>
+        <a
+          class="write_select_list"
+          onClick={() => {
+            setCategory(data);
+          }}
+        >
+          {data}
+        </a>
+      </li>
+    );
+  });
+
+  return (
+    <div class="write_select_popup_cove_on">
+      <div class="write_select_popup">
+        <ul>
+          <li>
+            <div class="write_select_popup_title">
+              분야 선택
+              <span class="write_select_popup_back">
+                <img
+                  src={BackImg}
+                  alt="back.png"
+                  onClick={selectModalHandler}
+                />
+              </span>
+            </div>
+            <ul class="write_select_li">{selectList}</ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default InquirySelectModal;
