f67a591490faf67b89f86efaf02729e04cf8b029
sangjun <sgbo5003@naver.com>
2021-08-09 17:27:48 +0900

    문의하기 기능 구현

--- src/routes/Setting/Inquiry.js
+++ src/routes/Setting/Inquiry.js
@@ -1,16 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import questionImg from "../../images/question_tab_btn.png";
 import emailCheckedImg from "../../images/email_checked.png";
 import emailCheckImg from "../../images/email_check.png";
 import selectBackImg from "../../images/select_back.png";
 import { Link } from "react-router-dom";
+import InquirySelectModal from "../../components/InquirySelectModal";
 
 const inquiry = () => {
-  const [emailChecked, setEmailChecked] = useState(false);
+  const [email, setEmail] = useState(""); // 이메일 주소
+  const [content, setContent] = useState(""); // 내용
+  const [category, setCategory] = useState(""); // 질문유형 선택 카테고리
+  const [emailChecked, setEmailChecked] = useState(false); // 이메일박스 체크
+  const [selectModalOn, setSelectModalOn] = useState(false); // 질문 유형 선택창 체크
+  const [buttonOn, setButtonOn] = useState(false); // 버튼 disable & enable 변경을 위해 필요한 state
 
+  // 이메일주소 입력란 값 관리
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  };
+  // 내용 입력란 값 관리
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+    console.log(e.target.value);
+  };
+  // 이메일 정보 제공 동의 체크박스 변경
   const onEmailCheckHandler = () => {
     setEmailChecked(!emailChecked);
   };
+  // 질문 유형 선택 모달 on & off 변경
+  const selectModalHandler = () => {
+    setSelectModalOn(!selectModalOn);
+  };
+  const checkButtonOn = () => {
+    if (email == "" || category == "" || content == "") {
+      setButtonOn(false);
+    } else {
+      setButtonOn(true);
+    }
+  };
+
+  // 버튼 변경 & state 변경 실시간 감지
+  useEffect(() => {
+    checkButtonOn();
+  });
+
   return (
     <div className="wap inquiry_wap">
       <div className="inquiry_content">
@@ -32,6 +66,8 @@ const inquiry = () => {
                   type="email"
                   name="checked_email"
                   placeholder="이메일 주소"
+                  value={email}
+                  onChange={onChangeEmail}
                   maxlength="20"
                   className="comment_inquiry_group inq_email_text inq_text_checked inquiry_text_box"
                 />
@@ -47,16 +83,29 @@ const inquiry = () => {
                   type="text"
                   name="comment_select"
                   placeholder="질문 유형 선택"
+                  value={category}
                   disabled
                   className="comment_inquiry_group inq_select_text inq_text_checked inquiry_text_box"
                 />
-                <a className="comment_select_img_cove">
+                <a
+                  className="comment_select_img_cove"
+                  onClick={selectModalHandler}
+                >
                   <img
                     className="comment_select_img"
                     src={selectBackImg}
                     alt="select_back.png"
                   />
                 </a>
+                {selectModalOn ? (
+                  <InquirySelectModal
+                    selectModalHandler={selectModalHandler}
+                    category={category}
+                    setCategory={setCategory}
+                  />
+                ) : (
+                  ""
+                )}
               </form>
             </div>
             <div className="inq_que_group">
@@ -65,6 +114,8 @@ const inquiry = () => {
                   type="text"
                   name="checked_write"
                   placeholder="내용"
+                  value={content}
+                  onChange={onChangeContent}
                   className="coment_inquiry inq_write_text inq_text_checked inquiry_text_box"
                 ></textarea>
                 <div
@@ -105,16 +156,16 @@ const inquiry = () => {
           <input
             type="submit"
             name="inq_submit_off"
-            className="inq_submit_off"
+            className={`inq_submit_${buttonOn ? "on" : "off"}`}
             value="문의글 보내기"
             disabled
           />
-          <input
+          {/* <input
             type="submit"
             name="inq_submit_on"
             className="inq_submit_on"
             value="문의글 보내기"
-          />
+          /> */}
         </div>
       </div>
     </div>
