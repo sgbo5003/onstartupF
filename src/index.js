1a73b7757ee104dd655a2a5130f643e5cd4fd4c0
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-08-11 17:15:51 +0900

    setting redux
    리덕스 세팅

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import combineReducers from './modules/index';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import combineReducers from "./modules/index";
 
 // axios.defaults.baseURL = "http://15.164.227.114/web/src/php/get_info.php?comment=1";
-axios.defaults.baseURL = "https://api.on-startup.co.kr/";
+// axios.defaults.baseURL = "https://api.on-startup.co.kr/";
 
 // refreshToken cookie를 주고받을 수 있다.
 // axios.defaults.withCredentials = true;
 
-const store = createStore(combineReducers, window.__REDUX_DEVTOOLS_EXTENSIOM__ && window.__REDUX_DEVTOOLS_EXTENSIOM__());
+const store = createStore(
+  combineReducers,
+  window.__REDUX_DEVTOOLS_EXTENSIOM__ && window.__REDUX_DEVTOOLS_EXTENSIOM__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
