cb2f1cdcc872df10a305ca6ec4ff0d33706fc895
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-08-11 17:14:39 +0900

    Mapping
    매핑

--- src/components/Qmenu.js
+++ src/components/Qmenu.js
@@ -3,32 +3,39 @@ import { Link } from "react-router-dom";
 import tailImg from "../images/tail.png";
 
 const Qmenu = (props) => {
+  const qmenuList = [
+    {
+      link: "/AccountManagement",
+      text: "계정관리",
+    },
+    {
+      link: "/ChangePassword",
+      text: "비밀번호 변경",
+    },
+    {
+      link: "/Inquiry",
+      text: "문의하기",
+    },
+    {
+      link: "/Notice",
+      text: "공지사항",
+    },
+  ];
   return (
     <div className={props.class}>
       {/* <div className={props.class} ref={props.outside}> */}
       <span className={props.tail}>
         <img className="tail_img" src={tailImg} alt="tail.png" />
       </span>
-      <p className="Qmenu_menu_cove">
-        <Link to="/AccountManagement" className="Qmenu_menu">
-          계정관리
-        </Link>
-      </p>
-      <p className="Qmenu_menu_cove">
-        <Link to="/ChangePassword" className="Qmenu_menu">
-          비밀번호 변경
-        </Link>
-      </p>
-      <p className="Qmenu_menu_cove">
-        <Link to="/Inquiry" className="Qmenu_menu">
-          문의하기
-        </Link>
-      </p>
-      <p className="Qmenu_menu_cove">
-        <Link to="/Notice" className="Qmenu_menu">
-          공지사항
-        </Link>
-      </p>
+      {qmenuList.map((data, index) => {
+        return (
+          <p className="Qmenu_menu_cove" key={index}>
+            <Link to={data.link} className="Qmenu_menu">
+              {data.text}
+            </Link>
+          </p>
+        );
+      })}
     </div>
   );
 };
