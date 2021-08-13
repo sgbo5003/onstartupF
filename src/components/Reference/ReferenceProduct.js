3b507d64cbae04a5cdcf26ec7966e40fee4214b4
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-08-13 17:54:25 +0900

    make function change menu state
    메뉴 상태를 바꾸는 함수 구현

--- src/components/Reference/ReferenceProduct.js
+++ src/components/Reference/ReferenceProduct.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReferenceSidebar from "./ReferenceSidebar";
 import gallery1 from "../../images/port_gallery6.png";
+import ReferenceChargeCoinShortageModal from "./ReferenceChargeCoinShortageModal";
+import ReferenceChargeCoinConfirmModal from "./ReferenceChargeCoinConfirmModal";
 const ReferenceProduct = () => {
+  const [referenceChargeCoinModalOn, setReferenceChargeCoinModalOn] =
+    useState(false);
+  const [menuClicked, setMenuClicked] = useState(false); // 메뉴 클릭 감지 state
+
+  // 모달 핸들러
+  const modalHandler = () => {
+    setReferenceChargeCoinModalOn(true);
+  };
+  // 문의하기 메뉴 핸들러
+  const inquiryMenuHandler = () => {
+    setMenuClicked(true);
+  };
+
+  // 상품설명 메뉴 핸들러
+  const explainMenuHandler = () => {
+    setMenuClicked(false);
+  };
+
   return (
     <>
       <ReferenceSidebar />
@@ -15,7 +35,10 @@ const ReferenceProduct = () => {
             <h3>소제목 소제목 소제목 소제목</h3>
             <p>리뷰 0</p>
             <div>
-              <button className="reference_product_purchase_button">
+              <button
+                className="reference_product_purchase_button"
+                onClick={modalHandler}
+              >
                 00코인으로 구매하기
               </button>
             </div>
@@ -25,12 +48,22 @@ const ReferenceProduct = () => {
           <div className="mypage_profiles_tab_menu_cove">
             <ul className="mypage_profiles_tab_cove">
               <li className="mypage_profiles_list my_comment_tab">
-                <a className="mypage_profiles_tab_btn my_comment_tab">
+                <a
+                  className={`mypage_profiles_tab_btn my_comment_tab ${
+                    menuClicked ? "mypage_profiles_tab_btn_active" : ""
+                  }`}
+                  onClick={inquiryMenuHandler}
+                >
                   문의하기
                 </a>
               </li>
               <li className="mypage_profiles_list">
-                <a className="mypage_profiles_tab_btn my_port_tab mypage_profiles_tab_btn_active">
+                <a
+                  className={`mypage_profiles_tab_btn my_comment_tab ${
+                    menuClicked ? "" : "mypage_profiles_tab_btn_active"
+                  }`}
+                  onClick={explainMenuHandler}
+                >
                   상품설명
                 </a>
               </li>
@@ -38,6 +71,7 @@ const ReferenceProduct = () => {
           </div>
         </div>
       </div>
+      {referenceChargeCoinModalOn ? <ReferenceChargeCoinConfirmModal /> : ""}
     </>
   );
 };
