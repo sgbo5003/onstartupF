55fbc1fc828a967d693b9c0e041a50f03432785f
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-08-17 15:53:34 +0900

    add cancel button function
    취소버튼에 기능 추가

--- src/components/Reference/ReferenceChargeCoinConfirmModal.js
+++ src/components/Reference/ReferenceChargeCoinConfirmModal.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-const ReferenceChargeCoinConfirmModal = () => {
+const ReferenceChargeCoinConfirmModal = (props) => {
+  const { setReferenceChargeCoinModalOn } = props;
+
+  const ModalClosedHandler = () => {
+    setReferenceChargeCoinModalOn(false);
+  };
   return (
     <div className="reference_charge_modal_container">
       <div className="reference_charge_modal_box">
         <div className="reference_charge_modal_box_top">구매하시겠습니까?</div>
         <div className="reference_charge_modal_box_bottom">
           <button className="reference_charge_modal_box_button">확인</button>
-          <button className="reference_charge_modal_box_button">취소</button>
+          <button
+            className="reference_charge_modal_box_button"
+            onClick={ModalClosedHandler}
+          >
+            취소
+          </button>
         </div>
       </div>
     </div>
