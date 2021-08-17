526290c317f83da63f2cf5e59ad2ff88c61487c6
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-08-17 19:08:22 +0900

    classname & css 수정

--- src/components/Write/WritePortfolioModal.js
+++ src/components/Write/WritePortfolioModal.js
@@ -3,7 +3,7 @@ const WritePortfolioModal = (props) => {
   return (
     // <div className={props.classOnOff}>
     <div className="write_comment_popup_cove_on">
-      <div className="write_comment_popup">
+      <div className="write_portfolio_modal_popup">
         <h3 className="write_comment_title">
           포트폴리오 등록 시에
           <br /> 마이페이지 > 포트폴리오에 등록됩니다.
